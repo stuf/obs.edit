40f2f1c208a1e0f01af74d660cced3ba25ceba8f
Stefan Rimaila <stefan@rimaila.fi>
2018-04-03 14:56:03 +0300

    Stats component should now be nicer

---
+++ src/components/i18n/index.js
@@ -0,0 +1,9 @@
+import { fromClass } from 'karet';
+import * as RI from 'react-intl';
+
+export const Index = fromClass(RI.FormattedMessage);
+export const FormattedDate = fromClass(RI.FormattedDate);
+export const FormattedNumber = fromClass(RI.FormattedNumber);
+export const FormattedTime = fromClass(RI.FormattedTime);
+export const FormattedRelative = fromClass(RI.FormattedRelative);
+export const FormattedPlural = fromClass(RI.FormattedPlural);
--- src/components/stats.js
+++ src/components/stats.js
@@ -2,27 +2,43 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
 import * as R from 'ramda';
+import { FormattedNumber } from './i18n';
 import type { ComponentType } from 'react';
 import type { OutputMedia } from '../types/models';
 
 interface Props {
   stats: OutputMedia;
+  className?: string;
 }
 
-const formatters = {
-  frames: R.identity,
-  bytes: R.identity,
-  time: R.identity
+const humanizeTimeSpan = v => {
+  const hours = v / 3600;
+  const minutes = (v / 60) % 60;
+  const seconds = v % 60;
+
+  const str =
+    [hours, minutes, seconds]
+      .map(R.pipe(Math.floor, R.toString, R.when(x => x.length === 1, R.concat('0'))))
+      .join(':');
+
+  return str;
 };
 
-const Stats: ComponentType<Props> = ({ stats }) =>
-  <footer>
+const formatters = {
+  frames: v => <FormattedNumber value={v} />,
+  bytes: v => <FormattedNumber value={v} />,
+  time: humanizeTimeSpan
+};
+
+const Stats: ComponentType<Props> = ({ stats, className }) =>
+  <div className="Stats">
+    <dl className={U.cns(className)}>
     {U.seq(stats,
            U.toPairs,
            U.map(([k, v]) =>
-             <div>
-               {k}: {(formatters[k] || R.identity)(v)}
-             </div>))}
-  </footer>;
+             [<dt key={U.string`${k}-key`}>{k}</dt>,
+              <dd key={U.string`${k}-value`}>{(formatters[k] || R.identity)(v)}</dd>]))}
+    </dl>
+  </div>;
 
 export default Stats;
