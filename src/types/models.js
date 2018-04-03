3fa6fd81ca3f94993f9a40315bf8c7ac0a2f517c
Stefan Rimaila <stefan@rimaila.fi>
2018-04-03 20:57:31 +0300

    Improve styles for displaying sources

--- src/types/models.js
+++ src/types/models.js
@@ -5,3 +5,21 @@ export interface OutputMedia {
   bytes: number;
   frames: number;
 }
+
+export interface Source {
+  cx: number;
+  cy: number;
+  source_cx: number;
+  source_cy: number;
+  x: number;
+  y: number;
+  name: string;
+  volume: number;
+  render: boolean;
+  type: string;
+}
+
+export interface Scene {
+  name: string;
+  sources: Array<Source>;
+}
